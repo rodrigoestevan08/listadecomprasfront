75703877b597a56fb1d472f03ea26fba191005cc
Renan Rodrigo <renanrb@ime.usp.br>
2018-10-08 12:13:23 -0300

    Adicionar front simples para a lista de compras

---
+++ script.js
@@ -0,0 +1,63 @@
+const inputs = document.querySelectorAll('section input');
+
+function atualizar(){
+  fetch("http://localhost:5000/produtos").then(resposta => resposta.json()).then(dados => construirLista(dados.produtos)).catch(erro => console.log(`ERRO: ${erro}`));
+}
+
+function removerItem(event){
+  fetch(`http://localhost:5000/produto/${event.target.id}`, {
+    method: "DELETE"
+  }).then(resposta => resposta.json()).then(dados => {
+    console.log(dados);
+    atualizar();
+  }).catch(erro => console.log(`ERRO: ${erro}`));
+}
+
+function construirLista(listaDeProdutos){
+  let lista = document.createElement('ul');
+
+  for(let produto of listaDeProdutos){
+    let item = document.createElement('li');
+    let texto = document.createElement('p');
+    let botao = document.createElement('button');
+
+    texto.innerHTML = `${produto.quantidade} - ${produto.nome} - ${produto.marca}`;
+    botao.innerHTML = "Remover";
+    botao.id = produto._id;
+    botao.onclick = removerItem;
+
+    item.appendChild(texto);
+    item.appendChild(botao);
+
+    lista.appendChild(item);
+  }
+
+  document.querySelector('#lista').innerHTML = "";
+  document.querySelector('#lista').appendChild(lista);
+}
+
+document.querySelector('button').onclick = function(event){
+  if(!inputs[0].value || !Number(inputs[2].value)){
+    alert("Preencha nome e quantidade");
+    return;
+  }
+
+  const novoProduto = {
+    nome: inputs[0].value,
+    marca: inputs[1].value,
+    quantidade: Number(inputs[2].value)
+  }
+
+  fetch('http://localhost:5000/produto/novo', {
+    method: "POST",
+    body: JSON.stringify(novoProduto),
+    headers:{
+      "Content-Type": "application/json"
+    }
+  }).then(resposta => resposta.json()).then(dados => {
+    console.log(dados);
+    atualizar();
+  }).catch(erro => console.log(`ERRO: ${erro}`));
+}
+
+atualizar();
